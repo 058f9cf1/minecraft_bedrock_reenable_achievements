1c1ea18358a1f20da2d85c79cf4777d3198ff062
058f9cf1 <058f9cf1@tutanota.com>
2024-09-22 20:47:26 +0100

    Fixed not writing to file properly

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 # Re-enable Minecraft Bedrock Achievements
-A tool for allowing you to earn achievements on a Minecraft Bedrock world that has enabled cheats or used creative mode.
+A tool for allowing you to earn achievements on a Minecraft Bedrock world that has enabled cheats or used creative mode. After the program is run, cheats will be disabled and the default world game mode will be set to survival. Any players set to creative mode or changed gamerules will remain changed.
 > [!IMPORTANT]
-> This is a tool for worlds on 'Minecraft for Windows', not 'Minecraft: Java Edition' or any other Minecraft Bedrock version. After the program has completed, other Bedrock versions will be able to join the world and get achievements.
-
-## Dependencies
-Requires Python 3 to be installed.
+> This is a tool for worlds on 'Minecraft for Windows', not 'Minecraft: Java Edition' or any other Minecraft Bedrock version. After the program has completed, other players on any Bedrock version will be able to join the world and unlock achievements.
 
 ## Installation
+Download the latest releases from the releases section.
+
+## Running from source
+
+### Dependencies
+Requires Python 3 to be installed.
 
 Open a terminal and run the following commands (requires git to be installed): 
 ```
@@ -14,7 +17,7 @@ git clone https://github.com/058f9cf1/minecraft_bedrock_reenable_achievements.gi
 ```
 Alternatively, download the file 'minecraft_bedrock_reenable_achievements.py'.
 
-## Usage
+### Usage
 Open a terminal and run the following:
 ```
 cd minecraft_bedrock_reenable_achievements
@@ -22,4 +25,4 @@ python minecraft_bedrock_reenable_achievements.py
 ```
 Alternatively, and double-click on the file 'minecraft_bedrock_reenable_achievements.py'.
 
-After the program has completed, cheats will be disabled and you will be set to survival mode in the world you selected.
+The program runs in the terminal. After completion, the terminal will close and the selected world will be updated.
--- minecraft_bedrock_reenable_achievements.py
+++ minecraft_bedrock_reenable_achievements.py
@@ -5,31 +5,31 @@ def error(message):
 	raise SystemExit
 
 if __name__ == "__main__":
-	path = os.path.join(os.path.expanduser('~'), "AppData", "Local", "Packages", "Microsoft.MinecraftUWP_8wekyb3d8bbwe", "LocalState", "games", "com.mojang", "minecraftWorlds")
-	if not os.path.isdir(path):
-		error("Error: Minecraft directory not found. Have you got Minecraft for Windows installed?")
-	worlds = os.listdir(path)
-	if not len(worlds):
-		error("Error: No worlds detected. Have you started a world?")
-	print("Worlds found:")
-	for i in range(len(worlds)):
-		levelname = os.path.join(path, worlds[i], "levelname.txt")
-		with open(levelname) as f:
-			print(str(i + 1) + ".", f.readline())
-	selection = int(input("Enter world number > "))
-	if selection - 1 not in range(len(worlds)):
-		error("Error: Selection not valid")
-	data_path = os.path.join(path, worlds[selection - 1], "level.dat")
-	
-	
-	with open(data_path, "r+b") as f:
-		data = bytearray(f.read())
-	
-		pos = data.find(b'\x00GameType')
-		data[pos + 9] = 0
-		pos = data.find(b'commandsEnabled')
-		data[pos + 15] = 0
-		pos = data.find(b'hasBeenLoadedInCreative')
-		data[pos + 23] = 0
-	
-		f.write(data)
+        path = os.path.join(os.path.expanduser('~'), "AppData", "Local", "Packages", "Microsoft.MinecraftUWP_8wekyb3d8bbwe", "LocalState", "games", "com.mojang", "minecraftWorlds")
+        if not os.path.isdir(path):
+                error("Error: Minecraft directory not found. Have you got Minecraft for Windows installed?")
+        worlds = os.listdir(path)
+        if not len(worlds):
+                error("Error: No worlds detected. Have you started a world?")
+        print("Worlds found:")
+        for i in range(len(worlds)):
+                levelname = os.path.join(path, worlds[i], "levelname.txt")
+                with open(levelname) as f:
+                        print(str(i + 1) + ".", f.readline())
+        selection = int(input("Enter world number > "))
+        if selection - 1 not in range(len(worlds)):
+                error("Error: Selection not valid")
+
+        data_path = os.path.join(path, worlds[selection - 1], "level.dat")
+        with open(data_path, "r+b") as f:
+                data = bytearray(f.read())
+
+                pos = data.find(b'\x00GameType')
+                data[pos + 9] = 0
+                pos = data.find(b'commandsEnabled')
+                data[pos + 15] = 0
+                pos = data.find(b'hasBeenLoadedInCreative')
+                data[pos + 23] = 0
+        
+                f.seek(0)
+                f.write(data)
